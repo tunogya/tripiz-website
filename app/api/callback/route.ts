5476da9b75996f9a1fa2707b25ec9f0c8783bf8a
tunogya <20251942+tunogya@users.noreply.github.com>
2024-05-12 00:37:22 +0800

    update

--- app/api/callback/route.ts
+++ app/api/callback/route.ts
@@ -1,11 +1,23 @@
 import {NextRequest, NextResponse} from "next/server";
+import redis from "@/utils/redis";
 
 const GET = async (req: NextRequest) => {
-  return NextResponse.json({
-    success: true
-  }, {
-    status: 200
-  });
+  const body = await req.json();
+  try {
+    const decoded = atob(body);
+    await redis.set(body.sourceMessageId, decoded, {
+      ex: 60 * 60 * 24 * 30,
+    });
+    return NextResponse.json({
+      success: true
+    }, {
+      status: 200,
+    });
+  } catch (error) {
+    return NextResponse.json({error, success: false,}, {
+      status: 500,
+    });
+  }
 }
 
-export default GET
+export {GET}
--- app/api/images/generates/route.ts
+++ app/api/images/generates/route.ts
@@ -1,11 +1,43 @@
 import {NextRequest, NextResponse} from "next/server";
 
-const GET = async (req: NextRequest) => {
-  return NextResponse.json({
-    success: true
-  }, {
-    status: 200
-  });
+const QSTASH = `https://qstash.upstash.io/v1/publish/`;
+const DALL_E = "https://api.openai.com/v1/images/generations";
+const VERCEL_URL = "https://dalle-2-jade.vercel.app";
+
+const POST = async (req: NextRequest) => {
+  const { prompt } = await req.json();
+  try {
+    const response = await fetch(`${QSTASH + DALL_E}`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${process.env.QSTASH_TOKEN}`,
+        "upstash-forward-Authorization": `Bearer ${process.env.OPENAI_API_KEY}`,
+        "Content-Type": "application/json",
+        "Upstash-Callback": `${VERCEL_URL}/api/callback`,
+      },
+      body: JSON.stringify({
+        prompt,
+        n: 1,
+        size: "1024x1024",
+        response_format: "b64_json",
+      }),
+    });
+    const json = await response.json();
+    return NextResponse.json({
+      success: true,
+      id: json.messageId,
+    }, {
+      status: 202
+    });
+  } catch (error) {
+    return NextResponse.json({
+      success: false,
+      error: error,
+      type: "Internal server error"
+    }, {
+      status: 500
+    });
+  }
 }
 
-export default GET
+export {POST}
