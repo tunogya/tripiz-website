bbad198aa035f40ad495558b5792803ca01366f5
tunogya <20251942+tunogya@users.noreply.github.com>
2024-05-20 01:14:47 +0800

    update

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,13 +1,32 @@
 import {NextRequest} from "next/server";
+import mongodbClient from "../../../utils/mongodb";
 
 const GET = async (req: NextRequest) => {
   return new Response("Hello, Next.js!")
 }
 
 const POST = async (req: NextRequest) => {
+  const { title, content, author } = await req.json();
 
-
-  return new Response("Hello, Next.js!")
+  await mongodbClient.connect();
+  const result = await mongodbClient.db("core").collection("posts").insertOne({
+    title,
+    content,
+    author,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  })
+  if (result.acknowledged) {
+    return Response.json({
+      id: result.insertedId.id,
+    })
+  } else {
+    return Response.json({
+      error: "Something went wrong",
+    }, {
+      status: 500
+    })
+  }
 }
 
 export {
