558b76b3d41783e26714fb658c3e04d3fc9af008
tunogya <20251942+tunogya@users.noreply.github.com>
2024-05-20 13:50:43 +0800

    update

--- utils/type.ts
+++ utils/type.ts
@@ -1,4 +1,4 @@
-import {ObjectId, UUID, VectorDoc} from "@datastax/astra-db-ts";
+import {ObjectId, VectorDoc} from "@datastax/astra-db-ts";
 
 export interface Post extends VectorDoc {
   _id: ObjectId,
@@ -17,4 +17,22 @@ export interface Post extends VectorDoc {
   },
   createdAt: Date,
   updatedAt: Date,
+}
+
+export interface Comment extends VectorDoc {
+  _id: ObjectId,
+  parent_post_id: ObjectId,
+  user: string,
+  text: string,
+  entities?: {
+    hashtags?: string[],
+    media?: {
+      id: string,
+      url: string,
+      media_url: string,
+      media_url_https: string,
+      type: string,
+    }[],
+  },
+  createdAt: Date,
 }
