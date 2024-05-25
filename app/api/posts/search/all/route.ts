e57e715ab47c9decb93f8174abaa1f9675c3aaad
tunogya <20251942+tunogya@users.noreply.github.com>
2024-05-25 15:12:36 +0800

    update

--- app/api/posts/search/all/route.ts
+++ app/api/posts/search/all/route.ts
@@ -1,6 +1,8 @@
 import {NextRequest} from "next/server";
 import {connectToDatabase} from "@/utils/astradb";
 import {embedding} from "@/utils/embedding";
+import redis from "@/utils/redis";
+import { createHash } from 'crypto';
 
 const GET = async (req: NextRequest) => {
   const query = req.nextUrl.searchParams.get("query");
@@ -24,6 +26,17 @@ const GET = async (req: NextRequest) => {
     })
   }
 
+  const hash = createHash('sha256').update(`${user}:search:${query}`).digest('hex');
+
+  const cache = await redis.get(hash);
+
+  if (cache) {
+    return Response.json({
+      data: cache,
+      cached: true,
+    });
+  }
+
   const { db } = await connectToDatabase();
   const similarPosts = await db.collection("posts").find(
       {
@@ -37,11 +50,19 @@ const GET = async (req: NextRequest) => {
     )
     .toArray();
 
+  const data = similarPosts.map((item) => ({
+    ...item,
+    _id: item._id?.toString()
+  }));
+
+
+  await redis.set(hash, data, {
+    ex: 5 * 60 * 60,
+  })
+
   return Response.json({
-    data: similarPosts.map((item) => ({
-      ...item,
-      _id: item._id?.toString()
-    })),
+    data,
+    cached: false,
   })
 }
 
