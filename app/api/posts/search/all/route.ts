57a659337faf2ae8e24bfc0e44ce6473357ce881
tunogya <20251942+tunogya@users.noreply.github.com>
2024-05-25 14:08:29 +0800

    update

--- app/api/posts/search/all/route.ts
+++ app/api/posts/search/all/route.ts
@@ -1,8 +1,19 @@
 import {NextRequest} from "next/server";
 import {connectToDatabase} from "@/utils/astradb";
+import {embedding} from "@/utils/embedding";
 
 const GET = async (req: NextRequest) => {
   const query = req.nextUrl.searchParams.get("query");
+  const user = req.headers.get("Tripiz-User");
+  const signature = req.headers.get("Tripiz-Signature");
+
+  if (!user) {
+    return Response.json({
+      error: "Missing required fields: Tripiz-User, Tripiz-Signature"
+    }, {
+      status: 403
+    })
+  }
 
   if (!query) {
     return Response.json({
@@ -13,9 +24,24 @@ const GET = async (req: NextRequest) => {
   }
 
   const { db } = await connectToDatabase();
+  const similarPosts = await db.collection("posts").find(
+      {
+        user: user,
+      },
+      {
+        vector: await embedding(query),
+        limit: 10,
+        projection: { $vector: 0 },
+      }
+    )
+    .toArray();
 
-
-
+  return Response.json({
+    data: similarPosts.map((item) => ({
+      ...item,
+      _id: item._id?.toString()
+    })),
+  })
 }
 
 export {GET}
