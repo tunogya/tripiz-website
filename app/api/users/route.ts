0e82bd4040e45adee2c90190d3a305ae60454950
tunogya <20251942+tunogya@users.noreply.github.com>
2024-05-28 14:49:04 +0800

    update

---
+++ app/api/users/route.ts
@@ -0,0 +1,40 @@
+import {NextRequest} from "next/server";
+import {connectToDatabase} from "@/utils/astradb";
+
+const POST = async (req: NextRequest) => {
+  const { user, expoPushToken } = await req.json();
+
+  if (!expoPushToken || !user) {
+    return Response.json({
+      error: "Missing required fields: expoPushToken, user",
+    }, {
+      status: 400
+    })
+  }
+
+  const { db } = await connectToDatabase();
+
+  const result = await db.collection("users").updateOne({
+    user: user,
+  }, {
+    $set: {
+      expoPushToken: expoPushToken,
+    }
+  }, {
+    upsert: true,
+  })
+
+  if (result.modifiedCount || result.upsertedCount) {
+    return Response.json({
+      success: true,
+    })
+  } else {
+    return Response.json({
+      error: "Something went wrong",
+    })
+  }
+}
+
+export {
+  POST
+}
