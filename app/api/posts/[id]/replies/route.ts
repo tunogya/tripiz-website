477b11644f2181e948450017ed89925b151eedae
tunogya <20251942+tunogya@users.noreply.github.com>
2024-06-03 22:43:26 +0800

    updated

--- app/api/posts/[id]/replies/route.ts
+++ app/api/posts/[id]/replies/route.ts
@@ -3,6 +3,18 @@ import {connectToDatabase} from "@/utils/astradb";
 
 const GET = async (req: NextRequest, { params }: { params: { id: string } }) => {
   const id = params.id;
+
+  let max_results: number = Number(req.nextUrl.searchParams.get("max_results") || 20);
+  const skip: number | undefined = Number(req.nextUrl.searchParams.get("skip") || 0);
+
+  if (max_results > 20 || max_results <= 0) {
+    return Response.json({
+      error: "max_results should > 0 and <= 20",
+    }, {
+      status: 400,
+    })
+  }
+
   const { db } = await connectToDatabase();
 
   const result = db.collection("events").find({
@@ -11,8 +23,10 @@ const GET = async (req: NextRequest, { params }: { params: { id: string } }) => 
       $elemMatch: { $eq: ["e", id] }
     },
   }, {
+    limit: max_results,
+    sort: {created_at: -1},
+    skip: skip,
     projection: {
-      sig: 0,
       $vector: 0,
     }
   })
