df374b612cc2e3bab0c9e875145cd5baefd4c2c4
tunogya <20251942+tunogya@users.noreply.github.com>
2024-06-09 22:48:38 +0800

    updated

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -118,6 +118,9 @@ const POST = async (req: NextRequest) => {
     $vector,
   });
   if (result.insertedId) {
+    fetch(`https://tripiz.abandon.ai/api/posts/${id}/replies`, {
+      method: "POST",
+    });
     return Response.json({
       id: result.insertedId,
     });
