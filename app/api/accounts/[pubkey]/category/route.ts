f81d76bec5f559d4885240d6016d88bff555555c
tunogya <tunogya@qq.com>
2024-06-28 23:23:03 +0800

    update

---
+++ app/api/accounts/[pubkey]/category/route.ts
@@ -0,0 +1,46 @@
+import { NextRequest } from "next/server";
+import { connectToDatabase } from "@/utils/astradb";
+
+const GET = async (
+  req: NextRequest,
+  { params }: { params: { pubkey: string } },
+) => {
+  const key = req.nextUrl.searchParams.get("key");
+
+  if (!key) {
+    return Response.json(
+      {
+        error: "Missing required fields: key",
+      },
+      {
+        status: 400,
+      },
+    );
+  }
+
+  const { db } = await connectToDatabase();
+
+  const result = await db.collection("contents").find(
+    {
+      pubkey: params.pubkey,
+      key: key,
+    },
+  ).toArray();
+
+  // 将相同的value归集为一组
+  const grouped = result.reduce((acc, cur) => {
+    // @ts-ignore
+    if (acc[cur.value]) {
+      // @ts-ignore
+      acc[cur.value].push(cur.id);
+    } else {
+      // @ts-ignore
+      acc[cur.value] = [cur.id];
+    }
+    return acc;
+  }, {});
+
+  return Response.json(grouped);
+};
+
+export { GET };
