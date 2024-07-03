9cc468f3685e025ad468a39bb8169584d5118957
tunogya <tunogya@qq.com>
2024-07-04 01:19:16 +0800

    update

--- app/components/NavigationBar.tsx
+++ app/components/NavigationBar.tsx
@@ -92,7 +92,7 @@ const NavigationBar: FC<{
       </div>
       <div className="flex flex-row items-center space-x-4">
         <a
-          href="https://apps.apple.com/cn/app/tripiz-thought-recorder/id6484276400" target="_blank"
+          href="https://www.abandon.ai/blog/2024/06/29/" target="_blank"
           className="text-white text-sm hover:scale-105 py-1 px-4 flex flex-row items-center space-x-1">
           <svg data-encore-id="icon" fill="white" width={16} height={16} role="img" aria-hidden="true" viewBox="0 0 16 16"><path d="M4.995 8.745a.75.75 0 0 1 1.06 0L7.25 9.939V4a.75.75 0 0 1 1.5 0v5.94l1.195-1.195a.75.75 0 1 1 1.06 1.06L8 12.811l-.528-.528a.945.945 0 0 1-.005-.005L4.995 9.805a.75.75 0 0 1 0-1.06z"></path><path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-6.5a6.5 6.5 0 1 0 0 13 6.5 6.5 0 0 0 0-13z"></path></svg>
           <div>Install App</div>
