af3180c7ef8eac56b13573a6dbdcf98dcb8d2b8a
tunogya <tunogya@qq.com>
2024-07-03 19:36:03 +0800

    update

--- app/components/useUserInfo.tsx
+++ app/components/useUserInfo.tsx
@@ -1,24 +1,57 @@
-import { use, useEffect } from "react";
+import { use, useEffect, useState } from "react";
 import { useWebSocket } from "./WebSocketProvider";
 import { v4 as uuidv4 } from 'uuid';
 import { useIndexedDB } from "react-indexed-db-hook";
 
 const useUserInfo = (pubkey: string) => {
     const { send } = useWebSocket();
-    const db = useIndexedDB('ecents');
-    
-    // 查询indexdb是否有event
+    const { openCursor } = useIndexedDB("events");
+    const [event, setEvent] = useState(undefined);
+    const [name, setName] = useState("Anonymous");
+    const [picture, setPicture] = useState(undefined);
 
-    // 发送事件查询最新的userinfo
+    useEffect(() => {
+        const query = async () => {
+            openCursor((evt) => {
+                // @ts-ignore
+                var cursor = evt.target.result;
+                if (cursor) {
+                    if (cursor.value.kind === 0 && cursor.value.pubkey === pubkey) {
+                        setEvent(cursor.value);
+                        return;
+                    }
+                    cursor.continue();
+                }
+            })
+        }
+        query();
+    }, []);
+
+    useEffect(() => {
+        try {
+            // @ts-ignore
+            const info = JSON.parse(event?.content);
+            setName(info?.name);
+            setPicture(info?.picture);
+        } catch (e) {
+            console.log(e);
+        }
+    }, [event]);
+
     useEffect(() => {
         send(JSON.stringify([
             "REQ",
             uuidv4(),
             {
-              authors: [pubkey],
-              kinds: [0],
-              limit: 1,
+                authors: [pubkey],
+                kinds: [0],
+                limit: 1,
             },
-          ]))
+        ]))
     }, [pubkey]);
+
+    return {
+        name,
+        picture
+    }
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -24,12 +24,12 @@ export default function RootLayout({
   const pathname = usePathname();
 
   const MENU = [
-    {
-      label: "Home",
-      path: "",
-      icon: <svg data-encore-id="icon" role="img" width={24} height={24} fill="currentColor" viewBox="0 0 24 24"><path d="M12.5 3.247a1 1 0 0 0-1 0L4 7.577V20h4.5v-6a1 1 0 0 1 1-1h5a1 1 0 0 1 1 1v6H20V7.577l-7.5-4.33zm-2-1.732a3 3 0 0 1 3 0l7.5 4.33a2 2 0 0 1 1 1.732V21a1 1 0 0 1-1 1h-6.5a1 1 0 0 1-1-1v-6h-3v6a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V7.577a2 2 0 0 1 1-1.732l7.5-4.33z"></path></svg>,
-      selectedIcon: <svg data-encore-id="icon" role="img" aria-hidden="true" width={24} height={24} fill="currentColor" viewBox="0 0 24 24"><path d="M13.5 1.515a3 3 0 0 0-3 0L3 5.845a2 2 0 0 0-1 1.732V21a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-6h4v6a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V7.577a2 2 0 0 0-1-1.732l-7.5-4.33z"></path></svg>,
-    },
+    // {
+    //   label: "Home",
+    //   path: "",
+    //   icon: <svg data-encore-id="icon" role="img" width={24} height={24} fill="currentColor" viewBox="0 0 24 24"><path d="M12.5 3.247a1 1 0 0 0-1 0L4 7.577V20h4.5v-6a1 1 0 0 1 1-1h5a1 1 0 0 1 1 1v6H20V7.577l-7.5-4.33zm-2-1.732a3 3 0 0 1 3 0l7.5 4.33a2 2 0 0 1 1 1.732V21a1 1 0 0 1-1 1h-6.5a1 1 0 0 1-1-1v-6h-3v6a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V7.577a2 2 0 0 1 1-1.732l7.5-4.33z"></path></svg>,
+    //   selectedIcon: <svg data-encore-id="icon" role="img" aria-hidden="true" width={24} height={24} fill="currentColor" viewBox="0 0 24 24"><path d="M13.5 1.515a3 3 0 0 0-3 0L3 5.845a2 2 0 0 0-1 1.732V21a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-6h4v6a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V7.577a2 2 0 0 0-1-1.732l-7.5-4.33z"></path></svg>,
+    // },
     {
       label: "Search",
       path: "search",
