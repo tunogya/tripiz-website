73b5edcbe88a9dbf24a046b30df08961f2cbf1f0
tunogya <tunogya@qq.com>
2024-07-03 21:36:50 +0800

    update

--- app/library/page.tsx
+++ app/library/page.tsx
@@ -1,4 +1,32 @@
+'use client';
+import { v4 as uuidv4 } from 'uuid';
+import { hexToBytes } from "@noble/hashes/utils";
+import { useLocalStorage } from "@uidotdev/usehooks";
+import { getPublicKey } from "nostr-tools";
+import { useEffect, useMemo } from "react";
+import { useWebSocket } from "../components/WebSocketProvider";
+
 const Page = () => {
+    const [skHex, setSkHex] = useLocalStorage("skHex", "");
+    const { send } = useWebSocket();
+
+    const pubkey = useMemo(() => {
+        const sk = hexToBytes(skHex);
+        return getPublicKey(sk);
+    }, [skHex]);
+
+    useEffect(() => {
+        send(JSON.stringify([
+            "REQ",
+            uuidv4(),
+            {
+                authors: [pubkey],
+                kinds: [1],
+                limit: 20,
+            },
+        ]));
+    }, [pubkey]);
+
     return (
         <div className="px-6">
             <div className="space-x-2">
@@ -13,7 +41,7 @@ const Page = () => {
                 </button>
             </div>
             <div>
-                
+
             </div>
         </div>
     )
