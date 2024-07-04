db7f3b050c889e5b5c3dfa3d5202311c525ec038
tunogya <tunogya@qq.com>
2024-07-04 14:15:14 +0800

    update

---
+++ utils/db.model.ts
@@ -0,0 +1,24 @@
+import Dexie, { Table } from 'dexie';
+
+// table inteface
+export interface Event {
+  id: number;
+  kind: number;
+  pubkey: string;
+  created_at: number;
+  content: string;
+  tags: string[][];
+  sig: string;
+}
+
+export class DB extends Dexie {
+  events!: Table<Event>; 
+  constructor() {
+    super('DB');
+    this.version(1).stores({
+        events: '&id, *kind, *pubkey, created_at, content, tags, sig',  
+    });
+  }
+}
+
+export const db = new DB(); // export the db
