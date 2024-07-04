abfcb39e09306cc5aeed9604276811e4ac5f50af
tunogya <tunogya@qq.com>
2024-07-04 15:06:25 +0800

    update

--- app/components/PostCard.tsx
+++ app/components/PostCard.tsx
@@ -9,7 +9,7 @@ const PostCard: FC<{ event: any }> = ({ event }) => {
 
   return (
     <div className="w-full space-y-2 pb-3">
-      <div className={`${category === "reflections" ? "" : category === "dreams" ? "rounded-full" : "rounded-xl"} overflow-hidden`}>
+      <div className={`${category === "reflections" ? "" : category === "dreams" ? "rounded-full" : "rounded-3xl"} overflow-hidden`}>
         <img src={`https://tripiz.abandon.ai/api/autoglyphs?hash=0x${event.id}`} alt={""} width={240} height={240} />
       </div>
       <div className="line-clamp-2 text-[#A7A7A7] text-sm">
